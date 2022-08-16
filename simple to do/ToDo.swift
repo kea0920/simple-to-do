4812c542a30a6e9baf5cec1f87933ec6b6dc9829
kea <kea@bxscience.edu>
2022-08-15 21:29:53 -0400

    completed iteration 0!

---
+++ simple to do/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  simple to do
+//
+//  Created by wenjuan Qiu on 8/15/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+    init(){
+        
+    }
+    
+}
---
+++ simple to do/ToDoTableViewController.swift
@@ -0,0 +1,65 @@
+//
+//  To-DoTableViewController.swift
+//  simple to do
+//
+//  Created by wenjuan Qiu on 8/15/22.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos : [ToDo] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        toDos = createToDos()
+    }
+    
+    func createToDos() -> [ToDo] {
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        // important is set to false by default
+        
+        return [swift, dog]
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+          } else {
+            cell.textLabel?.text = toDo.name
+          }
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
